3109d1bd652db1b7920628cc7cbcd39a9b9c0a13
Kirils Kalinins <kirillram.kk@gmail.com>
2023-03-15 12:04:15 +0200

    realizeta picas sarakstu apskatisana

--- src/Kalinins_pica.java
+++ src/Kalinins_pica.java
@@ -2,116 +2,7 @@ import javax.swing.JOptionPane;
 import java.util.ArrayList;
 public class Kalinins_pica {
 	
-//	static void Account(){
-//		String Name, Surname, adress, pNumber;
-//		
-//		Name =  JOptionPane.showInputDialog(null, "Ievadiet jusu Vardu",
-//				"Account", JOptionPane.QUESTION_MESSAGE);
-//		Surname =  JOptionPane.showInputDialog(null, "Ievadiet jusu uzvardu",
-//				"Account", JOptionPane.QUESTION_MESSAGE);
-//		adress =  JOptionPane.showInputDialog(null, "Ievadiet jusu adresi",
-//				"Account", JOptionPane.QUESTION_MESSAGE);
-//		pNumber =  JOptionPane.showInputDialog(null, "Ievadiet jusu telefona numuru",
-//				"Account", JOptionPane.QUESTION_MESSAGE);
-//	}
-//	static void PizzaOrdering(){
-//String[] picasIzvele = {"pica Margarita", "Studentu pica", "Mafia", "Hawaian", "atpakal uz menu"};
-//		
-//		String izvele = (String) JOptionPane.showInputDialog(null, "Izvelies Picu!", "Picas Izvele",
-//				JOptionPane.QUESTION_MESSAGE, null, picasIzvele, picasIzvele[0]);
-//		
-//		switch(izvele){
-//		case "Margarita": 
-//			Margarita();
-//			
-//			break;
-//			
-//		case "Studentu":
-//			StudentuPica();
-//			break;
-//			
-//		case "Mafia":
-//			Mafia();
-//			break;
-//			
-//		case "Hawaian":
-//			Hawaian();
-//			break;
-//			
-//			
-//		case "atpakal uz menu":
-//			JOptionPane.showMessageDialog(null, "xxx", "Pizza Ordering",JOptionPane.INFORMATION_MESSAGE);
-//			break;
-//			
-//			
-//			
-//		}while(!izvele.equals("atpakal uz menu"));
-//	}
-//
-//	static void Margarita(){
-//		String[] margaritaVeidas = {"30cm - 9.00€", "40cm - 12.00€", "50cm - 15.00"};
-//		 String MargaritasIzvele;
-//		 MargaritasIzvele = (String) JOptionPane.showInputDialog(null, "izvelies izmeru!", "Margaritas izvele", JOptionPane.QUESTION_MESSAGE,
-//				null, margaritaVeidas, margaritaVeidas[0] );
-//		 ArrayList<String> picalist = new ArrayList<String>();
-//		 switch(MargaritasIzvele){
-//		 
-//		 case "30cm - 9.00€":
-//			 picalist.add("30cm - 9.00€");
-//			 break;
-//			 
-//		 case "40cm - 12.00€":
-//			 break;
-//			 
-//		 case "50cm - 15.00":
-//			 break;
-//			 
-//		 }
-//		
-//		
-//	}
-//	static void StudentuPica(){
-//		String[] margaritaVeidas = {"30cm - 9.00€", "40cm - 12.00€", "50cm - 15.00"};
-//		 String MargaritasIzvele;
-//		 MargaritasIzvele = (String) JOptionPane.showInputDialog(null, "izvelies izmeru!", "Margaritas izvele", JOptionPane.QUESTION_MESSAGE,
-//				null, margaritaVeidas, margaritaVeidas[0] );
-//		 switch(MargaritasIzvele){
-//		 
-//		 case "30cm - 8.00€":
-//			 break;
-//			 
-//		 case "40cm - 10.50€":
-//			 break;
-//			 
-//		 case "50cm - 13.00":
-//			 break;
-//			 
-//		 }
-//	}
-//			 
-//	static void Mafia(){
-//		String[] MafiaVeidas = {"30cm - 12.00€", "40cm - 15.00€", "50cm - 18€"};
-//		
-//		String MafiaIzvele = (String) JOptionPane.showInputDialog(null, "Izvelies Mafia picas izmeru",
-//				"Mafia", JOptionPane.QUESTION_MESSAGE, null, MafiaVeidas, MafiaVeidas[0]);
-//		switch(MafiaIzvele){
-//		
-//		case "30cm - 12.00€":
-//			break;
-//			
-//		case "40cm - 15.00€":
-//			break;
-//			
-//		case "50cm - 18€":
-//		}
-//	
-//			
-//	}
-//	static void Hawaian(){
-//		String[] HawaianVeidi = {"30 cm - 11.00€", "40cm - 13.00€", "50cm - 15€"};
-//		//String HawaianIzvele = (String)JOptionPane.showMessageDialog(null, "Izvelies Hawaian picas izmeru", );
-//	}
-//	
+
 	
 	public static ArrayList<Pica> picaList = new ArrayList<Pica>();
 	
@@ -126,28 +17,31 @@ public class Kalinins_pica {
 		String izmers = (String) JOptionPane.showInputDialog(null, "Izvelies Picas izmçru!", "Izvçlies picu!",
 						JOptionPane.QUESTION_MESSAGE, null, picasIzmers, picasIzvele[1]);
 		int izm;
+		double cena;
 		switch(izmers){
-			case "20cm": izm=20; break;
-			case "50cm": izm=50; break;
-			default: izm=30;
+			case "20cm": izm=20; cena=8.0;  break;
+					
+			case "50cm": izm=50; cena=12.0; break;
+			default: izm=30; cena=10.0; 
 		}
 			
 		Pica jauna;
 		switch(izvele){
 			case "Pica Margarita":
 			
-				jauna = new Pica("Pica Margarita", 6.00, izm, true, true, false, false, false);
+				jauna = new Pica("Pica Margarita", 8.00, izm, true, true, false, false, false);
 				picaList.add(jauna);
-				System.out.println(jauna.Izdruka());
+				
 			break;
 					
 			case "Studentu":
-				jauna = new Pica("Studentu pica", 8.00, izm, true, true, true, false, true);
+				jauna = new Pica("Studentu pica", 6.00, izm, true, true, true, false, true);
 				picaList.add(jauna);
 			break;
 					
 				case "Mafia":
-					
+					jauna = new Pica("Mafia", 6.00, izm, true, true, true, false, true);
+					picaList.add(jauna);
 					break;
 					
 				case "Hawaian":
@@ -158,7 +52,13 @@ public class Kalinins_pica {
 	}
 	
 	static void VisasPicas(){
-		System.out.println(picaList.get(0).Izdruka());
+		
+		String str = "";
+		
+		for(int i=0; i<picaList.size(); i++){
+			str+= picaList.get(i).getNos()+" "+picaList.get(i).getDiam()+" "+picaList.get(i).getCena()+"\n";
+		}
+		JOptionPane.showMessageDialog(null, str, "txt \n", JOptionPane.INFORMATION_MESSAGE);
 	}
 	
 	public static void main(String[] args) {
--- src/Pica.java
+++ src/Pica.java
@@ -1,4 +1,4 @@
-
+	
 public class Pica {
 	String nosaukums;
 	double cena;
@@ -19,11 +19,21 @@ public class Pica {
 		this.tomati = tomati;
 		this.pipari = pipari;
 		this.gala = gala;
+		
 	}
 		
 	public String Izdruka(){
 		String rez = this.nosaukums + "  " + this.diametrs + " cm - " + this.cena + " EUR" ;
 		return rez;
 	}
-
+	public String getNos(){
+	return nosaukums;
+	}
+	public int getDiam(){
+		return diametrs;
+	}
+	public double getCena(){
+		return cena;
+	}
+	
 }
